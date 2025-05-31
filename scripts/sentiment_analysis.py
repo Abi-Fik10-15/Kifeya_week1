07cca230d25fb243ea3f7b918965309c1d98a7bc
Abi-Fik 10-15 <99613677+Abi-Fik10-15@users.noreply.github.com>
2025-05-31 02:11:23 -0900

    Update sentiment_analysis.py

--- scripts/sentiment_analysis.py
+++ scripts/sentiment_analysis.py
@@ -1,9 +1,15 @@
-import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
 import nltk
 nltk.download('vader_lexicon')
 
-def apply_vader_sentiment(news_df, text_col='headline'):
+def add_vader_sentiment(df, text_col='headline'):
     sid = SentimentIntensityAnalyzer()
-    news_df['sentiment'] = news_df[text_col].apply(lambda x: sid.polarity_scores(str(x))['compound'])
-    return news_df
+    df['sentiment'] = df[text_col].apply(lambda x: sid.polarity_scores(str(x))['compound'])
+    return df
+
+def compute_daily_sentiment(news_df, date_col='date'):
+    daily_sentiment = news_df.groupby(date_col)['sentiment'].mean().reset_index()
+    daily_sentiment.rename(columns={'sentiment': 'avg_daily_sentiment'}, inplace=True)
+    return daily_sentiment
+
